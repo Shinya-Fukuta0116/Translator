a2c6c634e5566ff07d4f87ccad77817ddb5deb34
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2022-12-29 07:12:42 +0900

    complete

--- js/app.js
+++ js/app.js
@@ -1,7 +1,9 @@
 const fromText = document.querySelector('.from-text'),
+  ToText = document.querySelector('.to-text'),
   selectTag = document.querySelectorAll('select'),
+  exchangeIcon = document.querySelector('.exchange'),
   translateBtn = document.querySelector('button');
-
+icons = document.querySelectorAll('.row i')
 selectTag.forEach((tag, id) => {
   for (const country_code in countries) {
 
@@ -16,9 +18,47 @@ selectTag.forEach((tag, id) => {
   }
 });
 
+exchangeIcon.addEventListener('click', () => {
+  let tempText = fromText.value;
+  tempLang = selectTag[0].value;
+  fromText.value = ToText.value;
+  selectTag[0].value = selectTag[1].value;
+  ToText.value = tempText;
+  selectTag[1].value = tempLang;
+});
+
+
 translateBtn.addEventListener('click', () => {
   let text = fromText.value,
     translateFrom = selectTag[0].value,
     translateTo = selectTag[1].value;
-  let apiUrl = ``;
+  if (!text) return;
+  ToText.setAttribute('placeholder', 'translating...');
+  let apiUrl = `https://api.mymemory.translated.net/get?q=${text}!&langpair=${translateFrom}|${translateTo}`;
+  fetch(apiUrl).then(res => res.json()).then(data => {
+    ToText.value = data.responseData.translatedText;
+    ToText.setAttribute('placeholder', 'translation');
+  });
 });
+
+icons.forEach(icon => {
+  icon.addEventListener('click', ({ target }) => {
+    if (target.classList.contains('fa-copy')) {
+      if (target.id == 'from') {
+        navigator.clipboard.writeText(fromText.value);
+      } else {
+        navigator.clipboard.writeText(ToText.value);
+      }
+    } else {
+      let utterance;
+      if (target.id == 'from') {
+        utterance = new SpeechSynthesisUtterance(fromText.value);
+        utterance.lang = selectTag[0].value;
+      } else {
+        utterance = new SpeechSynthesisUtterance(ToText.value);
+        utterance.lang = selectTag[1].value;
+      }
+      speechSynthesis.speak(utterance);
+    }
+  });
+})
